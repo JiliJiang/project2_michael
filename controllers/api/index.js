90bb0763385189007821cbf807eaa74995db74e1
JiliJiang <jiangjili@gmail.com>
2024-04-02 19:49:18 -0700

    first two pages

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const productRoutes = require('./productRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/users', userRoutes);
+router.use('/products', productRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
